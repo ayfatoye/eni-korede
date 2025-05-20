7d74c4beccaaff2aed9496f95e0b5a0aca172b45
ayfatoye <afatoye@tamu.edu>
2025-05-19 23:26:54 -0500

    fucking finally

--- src/components/NavComponent.tsx
+++ src/components/NavComponent.tsx
@@ -1,9 +1,16 @@
 function NavComponent() {
 
   return (
-    <>
-        <h2>ENIOLA & AKOREDE</h2>
-    </>
+    <div className="flex flex-col items-center justify-center h-[20vh]">
+  <h2 className="text-white text-xl mb-4">ENIOLA & AKOREDE</h2>
+
+  <ul className="flex flex-row list-none space-x-4 text-blue-500">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Schedule</a></li>
+    <li><a href="#">Registry</a></li>
+    <li><a href="#">RSVP</a></li>
+  </ul>
+</div>
   );
 }
 
