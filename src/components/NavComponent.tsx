9d870c47713f4e3088e4fbd8f38b02a1359233eb
ayfatoye <afatoye@tamu.edu>
2025-05-20 22:51:32 -0500

    done with the nav component for now

--- src/components/NavComponent.tsx
+++ src/components/NavComponent.tsx
@@ -1,16 +1,16 @@
 function NavComponent() {
 
   return (
-    <div className="flex flex-col items-center justify-center h-[20vh]">
-  <h2 className="text-white text-xl mb-4">ENIOLA & AKOREDE</h2>
+    <div className="flex flex-col items-center justify-between h-[20vh] w-[100vw] bg-white">
+      <h2 className="text-5xl text-[#717769] font-medium mt-2">ENIOLA & AKOREDE</h2>
 
-  <ul className="flex flex-row list-none space-x-4 text-blue-500">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">Schedule</a></li>
-    <li><a href="#">Registry</a></li>
-    <li><a href="#">RSVP</a></li>
-  </ul>
-</div>
+      <ul className="flex flex-row list-none space-x-4 mb-2 text-[#717769] font-normal text-2xl">
+        <li className="hover:underline underline-offset-[8px]"><a href="#">Home</a></li>
+        <li className="hover:underline underline-offset-[8px]"><a href="#">Schedule</a></li>
+        <li className="hover:underline underline-offset-[8px]"><a href="#">Registry</a></li>
+        <li className="hover:underline underline-offset-[8px]"><a href="#">RSVP</a></li>
+      </ul>
+    </div>
   );
 }
 
