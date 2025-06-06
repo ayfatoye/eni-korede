21a768c676548cdcbf48029c906bcfffa9e5017d
Bobobetterthanthefroyoyo <estherfatoye826@gmail.com>
2025-06-06 08:10:30 +0300

    adding link to the address

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -60,9 +60,9 @@ function Schedule() {
             <p className="text-[#717769] text-2xl md:text-4xl font-light">
               Enoch Event Center
             </p>
-            <p className="text-[#717769] text-xl md:text-2xl underline font-bold">
+            <a href="https://www.google.com/maps/place/Enoch's+Event+Center/@29.6889832,-95.6213606,17z/data=!3m1!4b1!4m6!3m5!1s0x8640dd7411d3de11:0x5c6f3c8f515870e0!8m2!3d29.6889833!4d-95.6164897!16s%2Fg%2F11fj0n11ty?entry=ttu&g_ep=EgoyMDI1MDYwMy4wIKXMDSoASAFQAw%3D%3D" className="text-[#717769] text-xl md:text-2xl underline font-bold">
               13350 Beechnut St, Houston, TX 77083
-            </p>
+            </a>
           </div>
         </div>
       </div>
