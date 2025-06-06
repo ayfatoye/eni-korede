9a8cdadba9d888b1c6507a519c29456aba2d89cf
Bobobetterthanthefroyoyo <estherfatoye826@gmail.com>
2025-06-07 02:53:43 +0300

    took out white weddig time on schedule.

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -25,21 +25,8 @@ function Schedule() {
             <p className="text-[#717769] text-2xl md:text-4xl font-medium">
               10:00 am - 02:00 pm
             </p>
-          </div>
-          <div className="mt-10 mb-10">
-            <div className="flex items-center justify-center mb-6">
-              <img
-                src="/icons8-hearts-50.png"
-                alt="Icon of two linked hearts"
-                className="w-9 h-9"
-              />
-            </div>
-            <p className="text-[#717769] text-2xl md:text-4xl font-bold md:font-medium">
-              WHITE WEDDING
-            </p>
-            <p className="text-[#717769] text-2xl md:text-4xl font-medium">
-              05:00 pm - 06:30 pm
-            </p>
+
+            <div className="mt-12 text-center"></div>
           </div>
           <div className="mt-0 mb-10">
             <div className="flex items-center justify-center mb-6">
